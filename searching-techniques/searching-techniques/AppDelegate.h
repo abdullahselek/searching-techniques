fdacb71f4894fc4312c56f11401598748380c77c
Abdullah Selek <abdullahselek@gmail.com>
2017-07-01 14:10:06 +0200

    create empty project

---
+++ searching-techniques/searching-techniques/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searching-techniques
+//
+//  Created by Abdullah Selek on 01/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ searching-techniques/searching-techniques/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  searching-techniques
+//
+//  Created by Abdullah Selek on 01/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

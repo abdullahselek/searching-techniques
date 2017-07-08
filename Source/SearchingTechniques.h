ad550052fce2bde0535e554b6c5a84787654a14b
Abdullah Selek <abdullahselek@gmail.com>
2017-07-08 21:29:15 +0200

    use foundation framework

--- Source/SearchingTechniques.h
+++ Source/SearchingTechniques.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Abdullah Selek. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SearchingTechniques.
 FOUNDATION_EXPORT double SearchingTechniquesVersionNumber;

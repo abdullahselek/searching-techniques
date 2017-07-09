a35fd7474118dcbad14f190cdce7c1a42aa81b98
Abdullah Selek <abdullahselek@gmail.com>
2017-07-09 20:11:21 +0200

    create tvOS target

---
+++ SearchingTechniques-tvOS/SearchingTechniques-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SearchingTechniques-tvOS.h
+//  SearchingTechniques-tvOS
+//
+//  Created by Abdullah Selek on 09/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SearchingTechniques-tvOS.
+FOUNDATION_EXPORT double SearchingTechniques_tvOSVersionNumber;
+
+//! Project version string for SearchingTechniques-tvOS.
+FOUNDATION_EXPORT const unsigned char SearchingTechniques_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchingTechniques_tvOS/PublicHeader.h>
+
+

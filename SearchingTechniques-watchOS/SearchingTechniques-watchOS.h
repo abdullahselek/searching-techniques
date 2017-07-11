3c1233b63487dfd2d3534f962035c1e1e9557a19
Abdullah Selek <abdullahselek@gmail.com>
2017-07-11 22:06:52 +0200

    init watchOS target

---
+++ SearchingTechniques-watchOS/SearchingTechniques-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  SearchingTechniques-watchOS.h
+//  SearchingTechniques-watchOS
+//
+//  Created by Abdullah Selek on 11/07/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SearchingTechniques-watchOS.
+FOUNDATION_EXPORT double SearchingTechniques_watchOSVersionNumber;
+
+//! Project version string for SearchingTechniques-watchOS.
+FOUNDATION_EXPORT const unsigned char SearchingTechniques_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchingTechniques_watchOS/PublicHeader.h>
+
+
